54deed2162b5f9601fd746870702af13a710962b
Jhonathan Sousa <ajhonathan73@gmail.com>
2024-07-18 15:11:04 -0300

    Realizado criação das funcionalidades do to-do-list

---
+++ assets/scripts/app.js
@@ -0,0 +1,126 @@
+// Selecionando input de tarefas
+const task = document.querySelector("#task");
+
+// Selecionando botão de criação de tarefas
+const createTask = document.querySelector("#createTask");
+
+const unregisteredTasks = document.querySelector('#unregisteredTasks');
+
+// Função para criar elementos de tarefas
+function createTaskButton(tasksItemDescription){
+
+  // Criar um input do tipo checkbox
+  const registeredTasksItemInput = document.createElement("input");
+  registeredTasksItemInput.setAttribute("type", "checkbox");
+  registeredTasksItemInput.setAttribute("class", "registeredTasksItemInput");
+
+  // Adicionar evento de mudança ao checkbox para atualizar as tarefas concluídas
+  registeredTasksItemInput.addEventListener('change', updateCompletedTasksCount);
+
+  // Criar um span para a marca do checkbox
+  const registeredTasksItemMark = document.createElement("span");
+  registeredTasksItemMark.setAttribute("class", "registeredTasksItemMark");
+
+  // Criar um span para a descrição da tarefa
+  const registeredTasksItemDescription = document.createElement("span");
+  registeredTasksItemDescription.setAttribute("class","registeredTasksItemDescription");
+  registeredTasksItemDescription.appendChild(document.createTextNode(tasksItemDescription));
+  
+  // Criar uma imagem de ícone de lixeira para deletar a tarefa
+  const trashIcon = document.createElement("img");
+  trashIcon.setAttribute("class", "icone-trash");
+  trashIcon.setAttribute("src", "assets/images/trash.svg");
+  trashIcon.setAttribute("width", "24");
+  trashIcon.setAttribute("height", "24");
+  trashIcon.setAttribute("alt", "Icone de lixeira para deletar tarefa");
+
+  // Adicionar evento de clique no ícone de lixeira para remover a tarefa 
+  trashIcon.addEventListener('click', () => {
+    taskList.remove();
+    updateTaskVisibility();
+    tasksCreatedCount();
+    updateCompletedTasksCount();
+  });
+
+  // Criar um label para agrupar os elementos da tarefa
+  const registeredTasksItem = document.createElement("label");
+  registeredTasksItem.setAttribute("class", "registeredTasksItem");
+  registeredTasksItem.append(registeredTasksItemInput,registeredTasksItemMark,registeredTasksItemDescription);
+
+  // Criar um elemento <li> para a tarefa
+  const taskList = document.createElement("li");
+  taskList.append(registeredTasksItem, trashIcon);
+
+  return taskList;
+}
+
+// Função para atualizar a visibilidade das tarefas não cadastradas
+function updateTaskVisibility() {
+  const registeredTasksId = document.querySelector("#registeredTasksId ul");
+  
+  if (registeredTasksId.childElementCount === 0) {
+    unregisteredTasks.classList.add('show');
+    unregisteredTasks.classList.remove('remove');
+  } else {
+    unregisteredTasks.classList.add('remove');
+    unregisteredTasks.classList.remove('show');
+  }
+}
+
+// Função para atualizar o número do tarefas criadas
+function tasksCreatedCount(){
+  const registeredTasksId = document.querySelector("#registeredTasksId ul");
+
+  // Selecionando e modificando o contador de tarefas concluidas
+  const tasksCreated = document.querySelector('#tasksCreated');
+  tasksCreated.innerHTML = registeredTasksId.childElementCount;
+}
+
+// Função para atualizar o número de tarefas concluídas
+function updateCompletedTasksCount() {
+  const registeredTasksItemInputs = document.querySelectorAll(".registeredTasksItemInput");
+  const completedTasks = Array.from(registeredTasksItemInputs).filter(input => input.checked).length;
+
+  const tasksCompleted = document.querySelector('#tasksCompleted');
+  tasksCompleted.innerHTML = completedTasks;
+}
+
+createTask.addEventListener("click", (event) => {
+
+  event.preventDefault();
+
+  // Pegando valor que é passado no input
+  const inputValue = task.value;
+  
+  if (inputValue === "") {
+
+    // Adicionando classe para preencher o input
+    task.classList.add('validateField');
+    task.setAttribute('placeholder' , 'Por favor, preencha o campo'); 
+
+  }else{
+
+    // Selecionar a área onde posso inserir a lista de tarefas
+    const registeredTasksId = document.querySelector("#registeredTasksId ul");
+    
+    // Criar um novo item de tarefa e adicionar à lista
+    const newTask = createTaskButton(inputValue);
+    registeredTasksId.appendChild(newTask);
+
+    // Atualizar a visibilidade das tarefas não cadastradas
+    updateTaskVisibility();
+
+    // Atualizar o número de tarefas criadas
+    tasksCreatedCount();
+
+     // Atualizar o número de tarefas concluídas
+    updateCompletedTasksCount();
+
+    // Limpa o campo de input após adicionar nova tarefa
+    task.value = '';
+
+    // Removendo class quando o input é preenchido
+    task.classList.remove('validateField');
+    task.setAttribute('placeholder' , 'Adicionar uma nova tarefa');
+  }
+});
